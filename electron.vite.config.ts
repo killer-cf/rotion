f84d53c3a79bc866a8cab2ae721930948889ff0b
killer-cf <costa.kilder@live.com>
2023-08-30 10:24:44 -0300

    app skeleton

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
+    define: {
+      'process.platform': JSON.stringify(process.platform),
+    },
     css: {
       postcss: {
         plugins: [
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,18 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
 
 export function App() {
   return (
-    <div className="min-h-screen w-screen bg-rotion-900 text-rotion-100"></div>
+    <div className="h-screen w-screen flex bg-rotion-900 text-rotion-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex flex-1 items-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
+    </div>
   )
 }
