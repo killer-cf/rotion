b94d098b637fee23fe72cf9b7cbd9b12bc457e76
killer-cf <costa.kilder@live.com>
2023-08-30 15:13:21 -0300

    Default layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,7 @@
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
 
 import { Routes } from './Routes'
 
 export function App() {
-  return (
-    <div className="h-screen w-screen flex bg-rotion-900 text-rotion-100">
-      <Sidebar />
-      <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
-
-        <Routes />
-      </div>
-    </div>
-  )
+  return <Routes />
 }
